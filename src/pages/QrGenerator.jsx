40d45a51861476582cd8914fff414882d82ba319
CristinaPineda <cristinadaspineda@yahoo.com.br>
2021-10-21 19:29:47 -0300

    render botoes

---
+++ src/pages/QrGenerator.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navbar from '../components/Navbar';
+
+export default function QrGenerator() {
+  return(
+    <div className="div-generator">
+      <Navbar />
+      <h1>
+        pagina gerador qrcode
+      </h1>
+    </div>
+  )
+}
---
+++ src/pages/QrReader.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navbar from '../components/Navbar';
+
+export default function QrReader() {
+  return(
+    <div className="div-generator">
+      <Navbar />
+      <h1>
+        pagina leitor qrcode
+      </h1>
+    </div>
+  )
+}
