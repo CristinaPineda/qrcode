bb2e9645bbe31548a1f92ca008dfe6669d6df4e7
CristinaPineda <cristinadaspineda@yahoo.com.br>
2021-10-21 18:06:33 -0300

    insere rotas

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,3 +1,15 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import Home from '../pages/Home';
+import QrGenerator from '../pages/QrGenerator';
+import QrReader from '../pages/QrReader';
 
+export default function Routes() {
+  return(
+    <Switch>
+      <Route exact path="/" component={ Home } />
+      <Route path="/qrgenerator" component={ QrGenerator } />
+      <Route exact path="/" component={ QrReader } />
+    </Switch>
+  )
+}
