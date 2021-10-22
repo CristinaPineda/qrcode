7bedc5a58f49b37117ef1789187870b107a4e0d9
CristinaPineda <cristinadaspineda@yahoo.com.br>
2021-10-21 22:38:25 -0300

    inclui header

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <header className="header">
+      <h1 className="h1-header">CrisCode</h1>
+      <div className="div-header">
+        <p>QRCODE</p>
+        <p>generator || reader</p>
+      </div>
+    </header>
+  );
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import Header from '../components/Header';
 import Navbar from '../components/Navbar';
 
 export default function Home() {
   return(
     <div className="div-home">
       <Navbar />
+      <Header />
     </div>
   )
 }
