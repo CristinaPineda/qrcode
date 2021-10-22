e3067f8c77922594ae7c2de5753fb607d3f94fef
CristinaPineda <cristinadaspineda@yahoo.com.br>
2021-10-22 16:54:51 -0300

    inclui imagens da home

--- src/components/SectionHome.jsx
+++ src/components/SectionHome.jsx
@@ -1,11 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import qrgenerator from '../images/qrgenerator.png';
+import qrreader from '../images/qrreader.png';
 
 export default function SectionHome() {
   return(
     <section className="section-home">
       <div>
-        <p>imagem do gerador qrcode</p>
-        <p>imagem do leitor qrcode</p>
+        <Link to="/qrgenerator"><img alt="imagem de um qrcode" src={ qrgenerator } width="300" height="300" /></Link>
+        <Link to="/qrreader"><img alt="imagem de um leitor de qrcode" src={ qrreader } width="300" height="300"/></Link>
       </div>
     </section>
   )
