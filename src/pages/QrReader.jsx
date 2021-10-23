c0ffb4a8e11fc3bf67cbbb616ececb94ceecf858
CristinaPineda <cristinadaspineda@yahoo.com.br>
2021-10-22 21:57:32 -0300

    estrutura pagina do leitor

--- src/pages/QrReader.jsx
+++ src/pages/QrReader.jsx
@@ -1,14 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Footer from '../components/Footer';
 import Navbar from '../components/Navbar';
+import QrScan from 'react-qr-reader';
 
 export default function QrReader() {
+  const [scan, setScan] = useState();
+
   return(
-    <div className="div-generator">
+    <div className="div-scanner">
       <Navbar />
-      <h1>
-        pagina leitor qrcode
-      </h1>
+      <section>
+        <div className="title-qr-scanner">
+          <h1>QR Scanner</h1>
+        </div>
+        <div className="div-p-scanner">
+          <p>Aponte a camera para ler o QRCode</p>
+        </div>
+        <div className="div-scanner">
+          
+        </div>
+        <div className="div-bts-scanner">
+          <input type="text" className="input-link-scanner" />
+          <button className="btn-ir">ir</button>
+        </div>
+      </section>
       <Footer />
     </div>
   )
