025d0a2d7b7c7a7d5cbff85800cdce8f3fc0d232
CristinaPineda <cristinadaspineda@yahoo.com.br>
2021-10-22 22:40:12 -0300

    leitor funcionando

--- src/pages/QrReader.jsx
+++ src/pages/QrReader.jsx
@@ -4,7 +4,17 @@ import Navbar from '../components/Navbar';
 import QrScan from 'react-qr-reader';
 
 export default function QrReader() {
-  const [scan, setScan] = useState();
+  const [scan, setScan] = useState('No result');
+
+  const handleScan = (img) => {
+    if(img) {
+      setScan(img);
+    }
+  }
+
+  const handleError = (err) => {
+    console.error(err);
+  }
 
   return(
     <div className="div-scanner">
@@ -16,14 +26,21 @@ export default function QrReader() {
         <div className="div-p-scanner">
           <p>Aponte a camera para ler o QRCode</p>
         </div>
-        <div className="div-scanner">
-          
-        </div>
-        <div className="div-bts-scanner">
-          <input type="text" className="input-link-scanner" />
-          <button className="btn-ir">ir</button>
-        </div>
+        <center>
+          <div className="div-scanner">
+            <QrScan
+              delay={300}
+              onError={handleError}
+              onScan={handleScan}
+              style={{ width: 300 }}
+              />
+          </div>
+        </center>
       </section>
+      <div className="div-btn-scanner">
+        <input type="text" className="input-link-scanner" value={ scan } />
+        <a href={scan} target="blank">ir</a>
+      </div>
       <Footer />
     </div>
   )
