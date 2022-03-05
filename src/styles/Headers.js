d0e5031051f8abce71ee90865e38db656c215262
CristinaPineda <cristinadaspineda@yahoo.com.br>
2022-03-05 19:56:16 -0300

    [refactor]:recycle: inicia refatoracao

--- src/styles/Headers.js
+++ src/styles/Headers.js
@@ -1,22 +1,19 @@
 import styled from "styled-components";
 
-export const Headers = styled.header`
+export const Headers = styled.header `
   color: #fff;
   text-align: center;
-
   p {
     color: #f666ae;
     font-size: 1.3rem;
   }
-
   @media (min-width: 768px) {
     display: flex;
     justify-content: space-around;
     align-items: center;
     flex-wrap: nowrap;
-
     p {
       font-size: 1.5rem;
     }
   }
-`
+`;
