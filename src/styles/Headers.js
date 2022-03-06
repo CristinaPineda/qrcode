f44962f06b96a76a82b2c8a2716f3bb6f9b41953
CristinaPineda <cristinadaspineda@yahoo.com.br>
2022-03-05 22:59:15 -0300

    [chore] :heavy_plus_sign: adiciona pacotes para configuracao de estilo

--- src/styles/Headers.js
+++ src/styles/Headers.js
@@ -1,12 +1,13 @@
 import styled from "styled-components";
 
-export const Headers = styled.header `
+export const Headers = styled.header`
   color: #fff;
   text-align: center;
   p {
     color: #f666ae;
     font-size: 1.3rem;
   }
+  
   @media (min-width: 768px) {
     display: flex;
     justify-content: space-around;
